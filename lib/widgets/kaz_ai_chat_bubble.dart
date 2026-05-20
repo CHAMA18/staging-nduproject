3ca0c6d1386c788d4b03e776907295cd41cafbb0
Chungu Chipimo Chama <chungu424@gmail.com>
2026-05-20 21:44:38 +0200

    latest changes

--- lib/widgets/kaz_ai_chat_bubble.dart
+++ lib/widgets/kaz_ai_chat_bubble.dart
@@ -526,7 +526,6 @@ class _KazAiChatPopupState extends State<_KazAiChatPopup>
 
       final body = jsonEncode({
         'model': OpenAiConfig.model,
-        'reasoning': {'effort': 'high'},
         'temperature': 0.7,
         'max_tokens': 800,
         'messages': messages,
